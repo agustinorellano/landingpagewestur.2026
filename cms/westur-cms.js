e6d3dcd975136fea0ebbf4f67601c0b9a5c49484
agustinorellano <agustinorellanogomez@gmail.com>
2026-08-10 12:38:07 -0300

    fix: update Apps Script URL to new deployment

--- cms/westur-cms.js
+++ cms/westur-cms.js
@@ -1,7 +1,7 @@
 // Westur CMS Loader
 // Reemplazá SCRIPT_URL con la URL que te da Apps Script al implementar
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwO6L1gbsFrSujp_f8_WZOkuwCw-CrXpUtHC4sAugR-aDrv_cC9-k_E1A_UIoydS931/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw4oYKrSHS7NZqIjXafTi0OPToEDPUTJKx9AMg5DRO3lqRDtfGxatraQ3pMVUiOO9Oyzg/exec';
 
 const WA = 'https://wa.me/541140919506';
 const waMsg = t => `${WA}?text=${encodeURIComponent(t)}`;
